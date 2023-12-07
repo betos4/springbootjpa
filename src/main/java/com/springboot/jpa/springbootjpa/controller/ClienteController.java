c982e1dd67ece650a46c7ae7f9d6a6d77a4b56c2
Roberto <rgallardoi@siccec.com.ec>
2023-12-07 16:21:09 -0500

    Cargando paginacion

--- src/main/java/com/springboot/jpa/springbootjpa/controller/ClienteController.java
+++ src/main/java/com/springboot/jpa/springbootjpa/controller/ClienteController.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springboot.jpa.springbootjpa.models.entity.Cliente;
 import com.springboot.jpa.springbootjpa.models.services.IClienteService;
+import com.springboot.jpa.springbootjpa.util.PageRender;
 
 import jakarta.validation.Valid;
 
@@ -33,8 +34,12 @@ public class ClienteController {
     public String listar(@RequestParam(name = "page", defaultValue = "0") int page,  Model model) {
         Pageable pageRequest = PageRequest.of(page, 4);
         Page<Cliente> clientes = clienteService.findAll(pageRequest);
+
+        PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
+
         model.addAttribute("titulo", "Listado de clientes");
         model.addAttribute("clientes", clientes);
+        model.addAttribute("page", pageRender);
 
         return "listar";
     }
---
+++ src/main/java/com/springboot/jpa/springbootjpa/util/PageItem.java
@@ -0,0 +1,16 @@
+package com.springboot.jpa.springbootjpa.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PageItem {
+    private int numero;
+    private boolean actual;
+
+}
